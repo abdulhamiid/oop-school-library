b2ee593064ae7aebb0e3bf0c17efa76f8f8ee68c
Abdulhamid <adioabdulhamid@yahoo.com>
2022-08-29 12:48:03 +0100

    Extract list logics into separate class(es).

--- app.rb
+++ app.rb
@@ -23,13 +23,11 @@ class App
   end
 
   def list_all_books
-    puts 'Database is empty! Add a book.' if @books.empty?
-    @books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}" }
+    List.list_all_books(@books)
   end
 
   def list_all_persons
-    puts 'Database is empty! Add a person.' if @persons.empty?
-    @persons.each { |person| puts "[#{person.class.name}] Name: #{person.name}, Age: #{person.age}, id: #{person.id}" }
+    List.list_all_persons(@persons)
   end
 
   def create_person
--- list.rb
+++ list.rb
@@ -13,4 +13,14 @@ class List
       end
     end
   end
+
+  def List.list_all_books(books)
+    puts 'Database is empty! Add a book.' if books.empty?
+    books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}" }
+  end
+
+  def List.list_all_persons(persons)
+    puts 'Database is empty! Add a person.' if persons.empty?
+    persons.each { |person| puts "[#{person.class.name}] Name: #{person.name}, Age: #{person.age}, id: #{person.id}" }
+  end
 end
