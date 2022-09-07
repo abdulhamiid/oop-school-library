8c1f52f04490daeaf7e922568c046e80e68aa139
lomantech <87063237+lomantech@users.noreply.github.com>
2022-09-07 13:48:41 +0300

    Add rental load method

--- readfile.rb
+++ readfile.rb
@@ -2,17 +2,20 @@ require 'json'
 require './book'
 require './student'
 require './teacher'
-
+require './rental'
 class Loaddata
-  def self.load_books(books)
+  def self.load_books
     # read from file
+    books =[]
     File.write('./data/books.json', []) unless File.exist?('./data/books.json')
     records = JSON.parse(File.read('./data/books.json'))
     records.each { |record| books << Book.new(record['title'], record['author']) }
+    books
   end
 
-  def self.load_persons(persons)
+  def self.load_persons
     # read from file
+    persons=[]
     File.write('./data/persons.json', []) unless File.exist?('./data/persons.json')
     records = JSON.parse(File.read('./data/persons.json'))
     records.each do |person|
@@ -32,5 +35,18 @@ class Loaddata
         )
       end
     end
+    persons
+  end
+
+  def self.load_rentals(persons)
+  # read rental file
+  rentals =[]
+  File.write('./data/rentals.json', []) unless File.exist?('./data/rentals.json')
+  data =JSON.parse(File.read('./data/rentals.json'))
+  data.each do |rent| 
+    person = persons.select{ |person| person.id == rent.id }
+    rentals << Rental.new(rent['date'], Book.new(rent.title, rent.author), person)
+  end
+  rentals
   end
 end
