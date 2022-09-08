3340b5f0f8cdae41966277077156337db90912e9
Abdulhamid <adioabdulhamid@yahoo.com>
2022-09-08 09:01:21 +0100

    Modify person variable and load rentals

--- readfile.rb
+++ readfile.rb
@@ -3,10 +3,11 @@ require './book'
 require './student'
 require './teacher'
 require './rental'
+
 class Loaddata
   def self.load_books
     # read from file
-    books =[]
+    books = []
     File.write('./data/books.json', []) unless File.exist?('./data/books.json')
     records = JSON.parse(File.read('./data/books.json'))
     records.each { |record| books << Book.new(record['title'], record['author']) }
@@ -15,38 +16,40 @@ class Loaddata
 
   def self.load_persons
     # read from file
-    persons=[]
+    people = []
     File.write('./data/persons.json', []) unless File.exist?('./data/persons.json')
     records = JSON.parse(File.read('./data/persons.json'))
-    records.each do |person|
-      if person['type'] == 'Student' 
-        persons << Student.new(
-          '', 
-          person['age'],
-          person['name'],
-          parent_permission: person['parent_permission']
-        )
-      elsif person['type'] == 'Teacher'
-        persons << Teacher.new(
-          nil,
-          person['age'],
-          person['name'],
-          parent_permission: person['parent_permission']
-        )
-      end
+    records.each do |record|
+      person = if record['type'] == 'Student'
+                 Student.new(
+                   nil,
+                   record['age'],
+                   record['name'],
+                   parent_permission: record['parent_permission']
+                 )
+               else
+                 Teacher.new('', record['age'], record['name'])
+               end
+      person.id = record['id']
+      people << person
     end
-    persons
+    people
   end
 
-  def self.load_rentals(persons)
-  # read rental file
-  rentals =[]
-  File.write('./data/rentals.json', []) unless File.exist?('./data/rentals.json')
-  data =JSON.parse(File.read('./data/rentals.json'))
-  data.each do |rent| 
-    person = persons.select{ |person| person.id == rent.id }
-    rentals << Rental.new(rent['date'], Book.new(rent.title, rent.author), person)
-  end
-  rentals
+  def self.load_rentals(books, persons)
+    # read rental file
+    rentals = []
+    File.write('./data/rentals.json', []) unless File.exist?('./data/rentals.json')
+    data = JSON.parse(File.read('./data/rentals.json'))
+    data.each do |rental|
+      date = rental['date']
+      book = books.select { |b| b.title == rental['book'] }[0]
+      person = persons.select { |ps| ps.name == rental['person'] }[0]
+
+      # create rental
+      rent = Rental.new(date, book, person)
+      rentals << rent
+    end
+    rentals
   end
 end
